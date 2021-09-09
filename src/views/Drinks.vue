97f33caaaab3671b414f2ef4352ffb5c9cae6016
BeataBarbara <skorusbeata@gmail.com>
2021-09-09 20:14:41 +0200

    alko

--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -1,9 +1,74 @@
 <template>
-  <div class="drinks">
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <h1>
+            Napoje %%
+        </h1>
+        <div class="danie">
+        <h2>Wino verde</h2>
+        <div class="opis-dania">
+            <p class="opis">
+              zielone wino <br>
+              Wytwarzane z niedojrzałych winogron.
+              Występuje w wersji białej (z zielonych winogron)               lub różowej (z czerwonych winogron).               Lekkie, orzeźwiające
+            </p>
+            <img alt="wino verde w wersji różowej, jakie można zakupić w ALDI" src="../assets/verde2.png" title="wino verde w wersji różowej, jakie można zakupić w ALDI"   />
+            <img alt="inne wino verde" src="../assets/verde1.png" title="inne wino verde"   />
+        </div>
+        </div>
+        <div class="danie">
+        <h2>Porto </h2>
+        <div class="opis-dania">
+            <p class="opis">
+              Ruby- słodkie, czerwone, o najniższej jakości <br>
+              Tawny- jest wytrawniejsze, traci większość nuty owocowej na rzecz większego udziału nuty korzennej i waniliowej <br>
+              Wzmacniane wino portugalskie, wytwarzane z winogron zbieranych w dolinie rzeki Douro (hiszp. Duero) na północy kraju, w jej środkowym biegu. Porto dojrzewa i jest butelkowane w mieście Vila Nova de Gaia
+            </p>
+            <img alt="Wino porto tawny" src="../assets/porto1.png" title="Wino porto tawny"   />
+        </div>
+        </div>
+        <div class="danie">
+        <h2> </h2>
+        <div class="opis-dania">
+            <p class="opis">
+
+            </p>
+            <img alt="" src="" title=""   />
+        </div>
+        </div>
+        <div class="danie">
+        <h2> </h2>
+        <div class="opis-dania">
+            <p class="opis">
+
+            </p>
+            <img alt="" src="" title=""   />
+        </div>
+        </div>
+        <div class="danie">
+        <h2> </h2>
+        <div class="opis-dania">
+            <p class="opis">
+
+            </p>
+            <img alt="" src="" title=""   />
+        </div>
+        </div>
+    </div>
 </template>
+
+<style>
+
+    p {
+        width: 75%;
+    }
+    .danie {
+        width:80%;
+        background-color: aquamarine;
+        padding: 5px;
+        border-radius: 20px;
+        margin: 10px;
+    }
+    .opis-dania {
+        display: flex;
+    }
+</style>
