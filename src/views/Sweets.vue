8057006f98c7d282a43cb8c76ef17ab90d5ea998
BeataBarbara <skorusbeata@gmail.com>
2021-09-09 20:01:44 +0200

    desery

--- src/views/Sweets.vue
+++ src/views/Sweets.vue
@@ -1,9 +1,44 @@
 <template>
-  <div class="sweets">
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <h1>
+            Desery
+        </h1>
+        <div class="danie">
+        <h2>Pastéis de nata</h2>
+        <div class="opis-dania">
+            <p class="opis">Inna jego nazwa to pasteis de Belem, od dzielnicy Lizbony, w której został przygotowany po raz pierwszy. 
+              przygotowywany z ciasta francuskiego i masy budyniowej, z dodatkiem cynamonu. <br>
+              Najlepiej więc zjadać je w nieprzyzwoitych ilościach w czasie pobytu w Portugalii. Obowiązkowo w duecie z kawką.
+            </p>
+            <img alt="moje piękne instagramowe Pastéis de nata" src="../assets/pasteldenatamoje.png" title="moje piękne instagramowe Pastéis de nata"   />
+        </div>
+        </div>
+        <div class="danie">
+        <h2>Areias de Cascais
+</h2>
+        <div class="opis-dania">
+            <p class="opis"> 
+              Kruche ciasteczka, przygotowywane z mąki, cukru, masła i skórki z limonki. <br>Pochodzą z Cascais (nadoceaniczny kurort w dystrykcie Lizbona).
+            </p>
+            <img alt="" src="" title=""   />
+        </div>
+        </div>
+    </div>
 </template>
+
+<style>
+
+    p {
+        width: 75%;
+    }
+    .danie {
+        width:80%;
+        background-color: aquamarine;
+        padding: 5px;
+        border-radius: 20px;
+        margin: 10px;
+    }
+    .opis-dania {
+        display: flex;
+    }
+</style>
